4cf2c9fdd2a1c0865520f784b6f917697fe2243d
nathaniel nosa <nathanielnarth@gmail.com>
2024-01-23 04:02:37 +0100

    database installation & setup

--- fpl/settings.py
+++ fpl/settings.py
@@ -67,11 +67,21 @@ WSGI_APPLICATION = 'fpl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'fpldb',
+       'USER': 'postgres',
+       'PASSWORD': 'admin',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
