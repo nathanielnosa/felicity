ce9e64b6cf5d3fa221b91691289ed6f44a23a5b2
nathaniel nosa <nathanielnarth@gmail.com>
2024-01-23 05:30:55 +0100

    registering model in admin

--- fpl/settings.py
+++ fpl/settings.py
@@ -123,6 +123,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'fpl/static')]
 
+# media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
