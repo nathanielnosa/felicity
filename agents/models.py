50105acf926c4f436c06a6583a94214a1a42c1a1
nathaniel nosa <nathanielnarth@gmail.com>
2024-01-26 01:42:08 +0100

    message alert and model for agent(still restructuring)

--- agents/models.py
+++ agents/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank= True)
     name = models.CharField(max_length=100)
     description = models.TextField( blank=True,null=True)
     phone = models.CharField(max_length=12, blank=True,null=True)
@@ -15,6 +17,23 @@ class Agent(models.Model):
     hired_at = models.DateTimeField(auto_now_add=True,blank=True,)
     updated_at = models.DateTimeField(auto_now=True,blank=True,)
     is_mvp = models.BooleanField(default=False)
+    is_agent = models.BooleanField(default=False,null=True,blank=True)
 
     def __str__(self):
         return self.name
+    
+class Message(models.Model):
+    sender = models.ForeignKey(Agent, on_delete=models.SET_NULL, null= True, blank=True)
+    recipient = models.ForeignKey(Agent, on_delete=models.SET_NULL, null= True, blank=True, related_name='messages')
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    is_read = models.BooleanField(default=False, null= True)
+    subject=models.CharField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+ 
+    class Meta:
+        ordering = ['is_read', '-created']
+
+    def __str__(self):
+        return self.subject
--- agents/views.py
+++ agents/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 
+# messages.add_message(request, messages.INFO, "Hello world.")
 # Create your views here.
 def index(request):
     return render(request, 'agents/agents.html')
